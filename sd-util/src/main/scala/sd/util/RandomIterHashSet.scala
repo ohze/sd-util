907f49c52305520e93e7c838b10576ae4711fb51
Gia Bao <giabao@sandinh.net>
2016-06-26 14:36:51 +0700

    add RandomIterHashSet

---
+++ sd-util/src/main/scala/sd/util/RandomIterHashSet.scala
@@ -0,0 +1,41 @@
+/** @author giabao
+  * created: 2013-02-01 20:59
+  * Copyright(c) 2011-2012 sandinh.com
+  */
+package sd.util
+
+import scala.collection.mutable
+import scala.util.Random
+
+/** extends HashSet to access field `table` */
+final class RandomIterHashSet[A] extends mutable.HashSet[A] {
+  import collection.Iterator
+  override def iterator: Iterator[A] = new Iterator[A] {
+    private val pivot = Random.nextInt(table.length) //table.length is always > 0
+    private var i = pivot - 1
+    @inline private def norm(): Unit = if (i < 0) i = table.length - 1
+    norm()
+    @inline private def nextI(): Unit = { i -= 1; norm() }
+    def hasNext: Boolean = {
+      while (i != pivot && (null == table(i))) nextI()
+      i != pivot
+    }
+    def next(): A =
+      if (hasNext) {
+        val ret = table(i)
+        nextI()
+        ret.asInstanceOf[A]
+      } else {
+        Iterator.empty.next()
+      }
+  }
+
+  def doSome[U](numUserToDo: Int, f: A => U): Unit = {
+    var n = numUserToDo
+    val i = iterator
+    while (n > 0 && i.hasNext) {
+      n -= 1
+      f(i.next())
+    }
+  }
+}
