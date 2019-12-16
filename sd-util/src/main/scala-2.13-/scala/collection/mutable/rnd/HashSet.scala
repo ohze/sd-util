dbd2c9efd10ea4c885f5d7f9ade30d8bcbcac38b
Bùi Việt Thành <thanhbv@sandinh.net>
2019-12-16 15:13:19 +0700

    simplify `scala-2.13-`'s rnd.HashSet

--- sd-util/src/main/scala-2.13-/scala/collection/mutable/rnd/HashSet.scala
+++ sd-util/src/main/scala-2.13-/scala/collection/mutable/rnd/HashSet.scala
@@ -4,27 +4,27 @@
   */
 package scala.collection.mutable.rnd
 
-import scala.collection.mutable
+import scala.collection.{AbstractIterator, mutable}
 import scala.util.Random
 
 /** Random Iterable HashSet */
 final class HashSet[A] extends mutable.HashSet[A] { // extends HashSet to access field `table`
   import collection.Iterator
-  override def iterator: Iterator[A] = new Iterator[A] {
-    private val pivot = Random.nextInt(table.length) //table.length is always > 0
-    private var i = pivot - 1
-    @inline private def norm(): Unit = if (i < 0) i = table.length - 1
-    norm()
-    @inline private def nextI(): Unit = { i -= 1; norm() }
+  override def iterator: Iterator[A] = new AbstractIterator[A] {
+    private[this] val pivot = Random.nextInt(table.length) //table.length is always > 0
+    private[this] val len1 = table.length - 1
+    private[this] var i = pivot - 1; if (i < 0) i = len1
     def hasNext: Boolean = {
-      while (i != pivot && (null == table(i))) nextI()
+      while (i != pivot && (null eq table(i))) {
+        i -= 1; if (i < 0) i = len1
+      }
       i != pivot
     }
     def next(): A =
       if (hasNext) {
         val ret = table(i)
-        nextI()
-        ret.asInstanceOf[A]
+        i -= 1; if (i < 0) i = len1
+        entryToElem(ret)
       } else {
         Iterator.empty.next()
       }
