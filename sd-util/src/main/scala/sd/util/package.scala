726af58eeeb74bf82528e298db006faab2b9b473
Bùi Việt Thành <thanhbv@sandinh.net>
2021-10-13 22:24:57 +0700

    Use @deprecated annotation instead of just @deprecated in scaladoc

--- sd-util/src/main/scala/sd/util/package.scala
+++ sd-util/src/main/scala/sd/util/package.scala
@@ -1,14 +1,14 @@
 package sd
 
 package object util {
-  /** @deprecated since 1.2.0 - use [[scala.collection.mutable.rnd.HashSet]] */
+  @deprecated("Use [[scala.collection.mutable.rnd.HashSet]]", "1.2.0")
   type RandomIterHashSet[A] = scala.collection.mutable.rnd.HashSet[A]
 
   implicit class DoSomeOps[A](private val it: Iterable[A]) extends AnyVal {
     /** not-so-optimized version of `it.take(count).foreach(f)` but ensure that a new Collection is not built
-     * @deprecated use `.iterator.take(count).foreach(f)`
      * @see [[scala.collection.mutable.rnd.HashSet]]
      */
+    @deprecated("use `.iterator.take(count).foreach(f)`", "1.2.0")
     def doSome[U](count: Int, f: A => U): Unit = {
       var n = count
       val i = it.iterator
